bc1e1fbcc1bfcd847ae4202789ea7c1c3e604d38
Shumon Huque <shuque@gmail.com>
2025-07-15 19:45:18 -0400

    Tweaks.

--- rirdata.py
+++ rirdata.py
@@ -14,7 +14,21 @@ def main():
     ip = sys.argv[1]
     obj = IPWhois(ip)
     result = obj.lookup_rdap()
-    print(json.dumps(result))
+
+    print(ip)
+    print("asn:", result['asn'])
+    print("rir:", result['asn_registry'])
+    print("country:", result['asn_country_code'])
+    print("description:", result['asn_description'])
+
+    print("network->handle:", result['network']['handle'])
+    print("network->cidr:", result['network']['cidr'])
+    print("network->ip_version:", result['network']['ip_version'])
+    print("network->type:", result['network']['type'])
+    print("network->name:", result['network']['name'])
+    print("network->country:", result['network']['country'])
+    print("network->parent_handle:", result['network']['parent_handle'])
+
 
 if __name__ == "__main__":
     main()
